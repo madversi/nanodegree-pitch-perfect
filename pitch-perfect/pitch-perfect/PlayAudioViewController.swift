03dd1a3d27f645c367edb8bba075852ea0bb7b4f
Helder M Adversi Jr <helderadversi@gmail.com>
2021-07-26 22:18:41 -0300

    feat: Adds audio effects by pressing buttons

--- pitch-perfect/pitch-perfect/PlayAudioViewController.swift
+++ pitch-perfect/pitch-perfect/PlayAudioViewController.swift
@@ -34,10 +34,25 @@ class PlayAudioViewController: UIViewController {
     }
 
     @IBAction func playSoundFor(button: UIButton) {
-
+        switch ButtonType(rawValue: button.tag) {
+        case .slow: playSound(rate: 0.5)
+        case .fast: playSound(rate: 1.5)
+        case .chipmunk: playSound(pitch: 1000)
+        case .vader: playSound(pitch: -1000)
+        case .echo: playSound(echo: true)
+        case .reverb: playSound(reverb: true)
+        default: playSound()
+        }
+        configureUI(.playing)
     }
 
     @IBAction func didTapStopButton(_ sender: AnyObject) {
-        
+        stopAudio()
+    }
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAudio()
     }
 }
